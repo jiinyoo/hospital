406908ff36a0f2aad53d6940ad4c947abd99789b
geoson <balsohn@naver.com>
2024-08-25 12:47:28 +0900

    admin doctor sogae

--- src/main/java/com/example/demo/admin/service/AdminMainServiceImpl.java
+++ src/main/java/com/example/demo/admin/service/AdminMainServiceImpl.java
@@ -5,9 +5,13 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import com.example.demo.admin.dto.AdminTestDto;
+import com.example.demo.admin.dto.DoctorDto;
 import com.example.demo.admin.mapper.AdminMainMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
 @Service
 @Qualifier("adm")
 
@@ -18,9 +22,17 @@ public class AdminMainServiceImpl implements AdminMainService {
 	
 	@Override
 	public String adminmain() {
-		ArrayList<AdminTestDto> list=mapper.test();
-		System.out.println(list.get(0));
+
 		return "/adminmain/index";
 	}
+
+	@Override
+	public String doctor(HttpServletRequest request,Model model) {
+		String doc_part = request.getParameter("doc_part");
+		model.addAttribute("doctorList",mapper.doctor(doc_part));
+		return "/adminmain/doctor";
+	}
+	
+	
 
 }
--- src/main/java/com/example/demo/client/mapper/ClientMainMapper.java
+++ src/main/java/com/example/demo/client/mapper/ClientMainMapper.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.demo.admin.dto.DoctorDto;
 import com.example.demo.client.dto.ClientTestDto;
 
 @Mapper
 public interface ClientMainMapper {
 
 	public ArrayList<ClientTestDto> clienttest();
-
-
+	
 }
