1479b8c0a4a1926096a4ec98d57a93dda33e0246
jiinyoo <yji3791@gmail.com>
2024-08-27 23:41:26 +0900

    jiin program register

--- src/main/java/kr/co/hospital/admin/dto/ProgramDto.java
+++ src/main/java/kr/co/hospital/admin/dto/ProgramDto.java
@@ -5,5 +5,5 @@ import lombok.Data;
 @Data
 public class ProgramDto {
 	int pro_id;
-	String pro_name, pro_info,teach_name,pro_time,pro_inwon,pro_part,pro_ju;
+	String pro_name, pro_info,teach_name,pro_time,pro_inwon,pro_part,pro_ju,pro_img;
 }
--- src/main/java/kr/co/hospital/admin/service/ProgramServiceImpl.java
+++ src/main/java/kr/co/hospital/admin/service/ProgramServiceImpl.java
@@ -1,5 +1,7 @@
 package kr.co.hospital.admin.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import kr.co.hospital.admin.mapper.ProgramMapper;
+import kr.co.hospital.admin.dto.ProgramDto;
 
 @Service
 @Qualifier("pc")
@@ -17,7 +20,9 @@ public class ProgramServiceImpl implements ProgramService {
 
 	@Override
 	public String program(HttpServletRequest request, Model model, HttpSession session) {
-		// TODO Auto-generated method stub
+		
+		ArrayList<ProgramDto> plist=mapper.programs();
+		model.addAttribute("plist",plist);
 		return "/admin/program/program";
 	}
 	
