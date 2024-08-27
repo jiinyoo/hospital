f3c0cd5c02a163f5abf0a33a223820835970d9c2
geoson <balsohn@naver.com>
2024-08-27 21:57:41 +0900

    medical

---
+++ src/main/java/kr/co/hospital/admin/service/MedicalServiceImpl.java
@@ -0,0 +1,41 @@
+package kr.co.hospital.admin.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import jakarta.servlet.http.HttpServletRequest;
+import kr.co.hospital.admin.dto.MedicalDto;
+import kr.co.hospital.admin.mapper.MedicalMapper;
+
+@Service
+@Qualifier("medi")
+public class MedicalServiceImpl implements MedicalService {
+
+	@Autowired
+	private MedicalMapper mapper;
+
+	@Override
+	public String mlist(Model model,HttpServletRequest request) {
+		String stype=request.getParameter("stype")==null?"user_name":request.getParameter("stype");
+		String sword=request.getParameter("sword")==null?"":request.getParameter("sword");
+		int page=request.getParameter("page")==null?1:Integer.parseInt(request.getParameter("page"));
+		int index=(page-1)*10;
+		int p=(page-1)/10;
+		int pstart=p*10+1;
+		int pend=pstart+9;
+		int chong=mapper.getChong(stype, sword);
+		
+		ArrayList<MedicalDto> mdto=mapper.mlist(index,stype,sword);
+		System.out.println(mdto.get(0).getMedi_id()); 
+		model.addAttribute("page",page);
+		model.addAttribute("pstart",pstart);
+		model.addAttribute("pend",pend);
+		model.addAttribute("chong",chong);
+		model.addAttribute("mdto",mdto);
+		return "/admin/medical/afterMedi";
+	}
+}
