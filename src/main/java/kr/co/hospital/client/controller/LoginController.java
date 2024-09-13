f2e3db4de77bc5efc835dfa6944444e53b92995e
zzdo2 <jdw1186@naver.com>
2024-09-14 00:42:45 +0900

    zzdo2

--- src/main/java/kr/co/hospital/client/controller/LoginController.java
+++ src/main/java/kr/co/hospital/client/controller/LoginController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -32,10 +34,30 @@ public class LoginController {
 		return service.loginOk(udto,session,request,response);
 	}
 	
-	@RequestMapping("/login/logout")
+	@RequestMapping("/main/logout")
 	public String logout(HttpSession session)
 	{
 		return service.logout(session);
-	}   
+	}
+	
+	 @GetMapping("/main/idSearch")
+	 public String idSearchPage() {
+	    return "client/login/idSearch";
+     }
+
+	 @PostMapping("/main/idSearch")
+	 public String idSearch(HttpServletRequest request, Model model) {
+	    return service.idSearch(request, model);
+	 }
+
+	 @GetMapping("/main/pwdSearch")
+	 public String pwdSearchPage() {
+	    return "client/login/pwdSearch";
+	 }
+
+	 @PostMapping("/main/pwdSearch")
+	 public String pwdSearch(HttpServletRequest request, Model model) {
+	    return service.pwdSearch(request, model);
+	 }
 
 }
